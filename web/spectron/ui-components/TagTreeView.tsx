662322d175158f2377bd6d5b26acaeaf522d8bc7
burton <burton@spinn3r.com>
2019-04-18 07:00:31 -0700

    ... broken as we dont' support typescript...

---
+++ web/spectron/ui-components/TagTreeView.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+
+import {Treebeard} from 'react-treebeard';
+
+export class TagTreeView extends React.Component<IProps, IState> {
+
+    constructor(props: IProps, context: any) {
+        super(props, context);
+    }
+
+    public render() {
+
+        return (
+
+            <div className="tag-tree-view">
+
+            </div>
+
+        );
+
+    }
+
+}
+
+
+interface IProps {
+}
+
+interface IState {
+}
+
+
